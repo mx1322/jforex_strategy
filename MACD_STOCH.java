ff63ae664909df8c9c4fb3016251e01897e07e37
mx1322 <46423448+mx1322@users.noreply.github.com>
2023-08-24 00:11:19 +0200

    Update MACD_STOCH.java to pass order

--- MACD_STOCH.java
+++ MACD_STOCH.java
@@ -1,6 +1,7 @@
 package jforex.strategies;
 
 import java.util.Arrays;
+import java.awt.Color;
 
 import com.dukascopy.api.*;
 import com.dukascopy.api.drawings.*;
@@ -31,7 +32,7 @@ public class MACD_STOCH implements IStrategy {
     private IChart chart;
     private IHistory history;
     private IChartObjectFactory factory;   
-    private Period period;   
+    private Period period;
 
 
     @Configurable("Amount")
@@ -50,15 +51,17 @@ public class MACD_STOCH implements IStrategy {
     private int shift = 0;
     public int counter = 0;
         
-    public int macdFastPeriod = 9;
-    public int macdSlowPeriod = 12;
-    public int macdSignalPeriod = 26;
-    public AppliedPrice appliedPrice = AppliedPrice.MEDIAN_PRICE;
-    
     public int fastPeriod = 12;
     public int slowPeriod = 26;
     public int signalPeriod = 9;
-
+    public AppliedPrice appliedPrice = AppliedPrice.MEDIAN_PRICE;
+        
+    private boolean stoch_buy = false;
+    private boolean stoch_sell = false;
+    private boolean macd_buy = false;
+    private boolean macd_sell = false;
+    private boolean SignalUP = false;
+    private boolean SignalDOWN = false;
 
     public void onStart(IContext context) throws JFException {
         engine = context.getEngine();
@@ -78,12 +81,40 @@ public class MACD_STOCH implements IStrategy {
 
     public void onBar(Instrument instrument, Period period, IBar askBar, IBar bidBar) throws JFException {
         if (instrument.equals(instrument) && period.equals(fixedPeriod)) {
+            //calculate Stoch
             double[] stochastic = indicators.stoch(instrument, this.period, side, fastKPeriod, slowKPeriod , slowKMaType, 
                     slowDPeriod, slowDMaType, shift);
-            print("STO params are "+"Instrument: " + instrument + ", Period: " + this.period + ", Side: " + side + ", FastKPeriod: " + fastKPeriod + ", SlowKPeriod: " + slowKPeriod + ", SlowKMaType: " + slowKMaType + ", SlowDPeriod: " + slowDPeriod + ", SlowDMaType: " + slowDMaType + ", Shift: " + shift);
+            //print("STO params are "+"Instrument: " + instrument + ", Period: " + this.period + ", Side: " + side + ", FastKPeriod: " + fastKPeriod + ", SlowKPeriod: " + slowKPeriod + ", SlowKMaType: " + slowKMaType + ", SlowDPeriod: " + slowDPeriod + ", SlowDMaType: " + slowDMaType + ", Shift: " + shift);
             
+            double[] stochastic_shift1 = indicators.stoch(instrument, this.period, side, fastKPeriod, slowKPeriod , slowKMaType, 
+                slowDPeriod, slowDMaType, 1);
+            
+            // Update stoch_buy and stoch_sell flags
+            if (!stoch_buy){
+                stoch_buy = (stochastic[0] > stochastic[1] && stochastic_shift1[0] < stochastic_shift1[1]);
+            }
+            if (!stoch_sell){
+                stoch_sell = (stochastic[0] < stochastic[1] && stochastic_shift1[0] > stochastic_shift1[1]);
+            }
+            
+            //calculate MACD
             double[] macd = indicators.macd(instrument, this.period, side, appliedPrice,
-              fastPeriod, slowPeriod, signalPeriod, shift);
+                fastPeriod, slowPeriod, signalPeriod, shift);
+              
+            double[] macd_shift1 = indicators.macd(instrument, this.period, side, appliedPrice,
+                fastPeriod, slowPeriod, signalPeriod, 1);
+
+            // Update macd_buy and macd_sell flags
+            if (!macd_buy){
+                macd_buy = (macd[0] > macd[2] && macd_shift1[0] < macd_shift1[2]);
+            }
+            if (!macd_sell){
+                macd_sell = (macd[0] < macd[2] && macd_shift1[0] > macd_shift1[2]);
+            }
+            
+            // Create unique keys for each signal
+            String signalUpKey = "signalUpKey" + bidBar.getTime();
+            String signalDownKey = "signalDownKey" + bidBar.getTime();
             
             StringBuilder macdstr = new StringBuilder("macd values are ");
             for (double element : macd) {
@@ -96,22 +127,38 @@ public class MACD_STOCH implements IStrategy {
                 stostr.append(String.format("%.1f", element)).append(" ");
             }
             
-            print(stostr.toString());
-            print(macdstr.toString());            
+            
+            // Create arrow up when both stoch_buy and macd_buy are true
+            if (stoch_buy && macd_buy && chart.get(signalUpKey) == null) {
+                ISignalUpChartObject signalUp = factory.createSignalUp(signalUpKey, bidBar.getTime(), bidBar.getLow());
+                signalUp.setColor(Color.GREEN);  // You can set the color of the arrow here
+                chart.add(signalUp);
+                stoch_buy = false;
+                macd_buy = false;
+                print(stostr.toString());
+                print(macdstr.toString());
+                submitOrder(OrderCommand.BUY, instrument, bidBar);
+            }
+
+            // Create arrow down when both stoch_sell and macd_sell are true
+            if (stoch_sell && macd_sell && chart.get(signalDownKey) == null) {
+                ISignalDownChartObject signalDown = factory.createSignalDown(signalDownKey, bidBar.getTime(), bidBar.getHigh());
+                signalDown.setColor(Color.RED);  // You can set the color of the arrow here
+                chart.add(signalDown);
+                stoch_sell = false;
+                macd_sell = false;
+                print(stostr.toString());
+                print(macdstr.toString());
+                submitOrder(OrderCommand.SELL, instrument, bidBar);
+            }            
         }
     }
 
-    private void createOrderOnStochastic(Instrument instrument, IBar bidBar, double[] stochastic) throws JFException {
-        OrderCommand orderCommand;
-        if ((stochastic[0] >= 80) && (stochastic[1] >= 80)) {
-            orderCommand = OrderCommand.SELL;
-            closeOppositeIfExist(orderCommand);
-            createOrder(instrument, bidBar, orderCommand);
-        } else if ((stochastic[0] <= 20) && (stochastic[1] <= 20) ) {
-            orderCommand = OrderCommand.BUY;
-            closeOppositeIfExist(orderCommand);
-            createOrder(instrument, bidBar, orderCommand);
+    private void submitOrder(OrderCommand orderCommand, Instrument instrument, IBar bidBar) throws JFException {
+        if (engine.getOrders().size() > 0) {
+            return;
         }
+        engine.submitOrder(getLabel(instrument), instrument, orderCommand, amount);
     }
 
     private void closeOppositeIfExist(OrderCommand command) throws JFException {
